33328ba83983195ef9a0dc5199af421a7655ff2a
Scott Gary Vaughan <scottgv@colorado.cs.byu.edu>
2016-01-30 13:19:15 -0700

    init Junit dir

---
+++ test/main/unitTestDriver.java
@@ -0,0 +1,10 @@
+package main;
+
+public class unitTestDriver {
+	 public static void main(String[] args){
+		 String[] testCases = new String[] {
+				 "shared.model.PlayerTest",
+		 };
+		 org.junit.runner.JUnitCore.main();
+	 }
+}
---
+++ test/shared/model/PlayerTest.java
@@ -0,0 +1,48 @@
+package shared.model;
+
+import org.junit.*;
+
+import shared.model.player.Player;
+
+public class PlayerTest {
+	@Before
+	public void setup(){
+		Player player = new Player(3);
+	}
+	
+	@After
+	public void tearDown(){
+		
+	}
+	
+	@Test
+	public void testBuyRoad(){
+		
+	}
+
+	@Test
+	public void testcanDoBuySettlement(){
+		
+	}
+	
+	@Test
+	public void testBuySettlement(){
+		
+	}
+	
+	@Test
+	public void testcanDoBuyCity(){
+		
+	}
+	
+	@Test
+	public void testBuyCity(){
+		
+	}
+	
+	@Test
+	public void testCanDoCollectResources(){
+		
+	}
+	
+}
