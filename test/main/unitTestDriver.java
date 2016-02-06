4f53d296399874973cab28d59b5b738b42a2829a
John Christopher Wiedemeier <jchrisw@colorado.cs.byu.edu>
2016-02-05 22:37:27 -0700

    Adding tests to main

--- test/main/unitTestDriver.java
+++ test/main/unitTestDriver.java
@@ -6,7 +6,9 @@ public class unitTestDriver {
 	 public static void main(String[] args){
 		 String[] testCases = new String[] {
 				 "shared.model.PlayerTest",
-				 "shared.model.BoardTest"
+				 "shared.model.BoardTest",
+				 "client.communication.ServerPollerTest",
+				 "client.communication.ServerProxyTest"
 		 };
 		 org.junit.runner.JUnitCore.main(testCases);
 	 }
