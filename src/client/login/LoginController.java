d55ef5362df6ca35fdaec629ba8ad134b21d70f4
Dallin Carl Andersen <dallinca@louisiana.cs.byu.edu>
2016-02-19 14:55:43 -0700

    added output messages to help with GUI programming, also added some message boxes for login sequence

--- src/client/login/LoginController.java
+++ src/client/login/LoginController.java
@@ -8,6 +8,7 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 import java.lang.reflect.*;
+
 import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
 
@@ -29,16 +30,19 @@ public class LoginController extends Controller implements ILoginController, Obs
 	public LoginController(ILoginView view, IMessageView messageView) {
 
 		super(view);
+		System.out.println("LoginController LoginController()");
 
 		this.messageView = messageView;
 	}
 	
 	public ILoginView getLoginView() {
+		System.out.println("LoginController getLoginView()");
 		
 		return (ILoginView)super.getView();
 	}
 	
 	public IMessageView getMessageView() {
+		System.out.println("LoginController getMessageView()");
 	
 		return messageView;
 	}
@@ -49,6 +53,7 @@ public class LoginController extends Controller implements ILoginController, Obs
 	 * @param value The action to be executed when the user logs in
 	 */
 	public void setLoginAction(IAction value) {
+		System.out.println("LoginController setLoginAction()");
 		
 		loginAction = value;
 	}
@@ -59,18 +64,21 @@ public class LoginController extends Controller implements ILoginController, Obs
 	 * @return The action to be executed when the user logs in
 	 */
 	public IAction getLoginAction() {
+		System.out.println("LoginController getLoginAction()");
 		
 		return loginAction;
 	}
 
 	@Override
 	public void start() {
+		System.out.println("LoginController start()");
 		
 		getLoginView().showModal();
 	}
 
 	@Override
 	public void signIn() {
+		System.out.println("LoginController signIn()");
 		
 		// TODO: log in user
 		ILoginView loginview = getLoginView();
@@ -91,14 +99,23 @@ public class LoginController extends Controller implements ILoginController, Obs
 	 * @return boolean, weather name is valid
 	 */
 	public boolean checkName(String registername){
+		System.out.println("LoginController checkName()");
 		if(registername.length() < 3 || registername.length() > 7){
+			getMessageView().setMessage("Your username must be between 3 and 7 characters long (inclusive)");
+			getMessageView().showModal();
+			System.out.println("incorrect length");
 			return false;
 		}
+		System.out.println("correct length");
 		for(int i = 0; i < registername.length(); i++){
 			char c = registername.charAt(i);
 			if(Character.isLetter(c) || Character.isDigit(c)|| c == '-' || c == '_'){ }
-			else
+			else {
+				getMessageView().setMessage("The username must be letters, digits, '_' or '-'");
+				getMessageView().showModal();
+				System.out.println("bad characters");
 				return false; 
+			}
 		}
 		return true;
 	}
@@ -109,7 +126,17 @@ public class LoginController extends Controller implements ILoginController, Obs
 	 * @return boolean, weather Password is valid
 	 */
 	public boolean checkPassword(String registerpassword, String repeatregisterpassword){
+		System.out.println("LoginController checkPassword()");
 		if(registerpassword.length() < 5){
+			getMessageView().setMessage("The password must be five or "
+					+ "more characters: letters, digits, _ and - ");
+			getMessageView().showModal();
+			return false;
+		}
+		// This is checking to make sure they are of the same length (without this we fail with "dallin" and "dallina" as the passwords)
+		if(registerpassword.length() != repeatregisterpassword.length()) {
+			getMessageView().setMessage("The passwords must be the same length");
+			getMessageView().showModal();
 			return false;
 		}
 		for(int i = 0; i < registerpassword.length(); i++){
@@ -118,10 +145,12 @@ public class LoginController extends Controller implements ILoginController, Obs
 			else{
 				getMessageView().setMessage("The password must be five or "
 						+ "more characters: letters, digits, _ and - ");
+				getMessageView().showModal();
 				return false;
 			}
 			if(registerpassword.charAt(i) != repeatregisterpassword.charAt(i)){
 				getMessageView().setMessage("The two passwords must match");
+				getMessageView().showModal();
 				return false;
 			}
 		}
@@ -130,6 +159,7 @@ public class LoginController extends Controller implements ILoginController, Obs
 	
 	@Override
 	public void register() {
+		System.out.println("LoginController register()");
 		ILoginView loginview = getLoginView();
 		
 		String registername = loginview.getRegisterUsername();
@@ -139,8 +169,11 @@ public class LoginController extends Controller implements ILoginController, Obs
 		if(checkName(registername)){
 			if(checkPassword(registerpassword, repeatregisterpassword)){
 				// If register succeeded
+
+				System.out.println("about to login");
 				getLoginView().closeModal();
 				loginAction.execute();
+				return;
 			}
 			else{
 				return;
@@ -149,13 +182,19 @@ public class LoginController extends Controller implements ILoginController, Obs
 		else{
 			getMessageView().setMessage("The username must be between three and "
 					+ "seven characters: letters, digits, _ and - ");
+			getMessageView().showModal();
 			return;
 		}
 	}
 
+	/**
+	 * TODO
+	 * 
+	 */
 	@Override
 	public void update(Observable o, Object arg) {
-		// TODO Auto-generated method stub
+		System.out.println("LoginController update()");
+		// Auto-generated method stub
 		
 	}
 
