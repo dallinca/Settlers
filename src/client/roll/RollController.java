d85924b0f49956db0292599a9c39a7bfca3cc9dd
Thomas Wilkinson <jazz.wookie@gmail.com>
2016-02-20 12:59:07 -0700

    the last of the system.out.println()'s

--- src/client/roll/RollController.java
+++ src/client/roll/RollController.java
@@ -22,31 +22,35 @@ public class RollController extends Controller implements IRollController, Obser
 	public RollController(IRollView view, IRollResultView resultView) {
 
 		super(view);
+		System.out.println("RollController RollController()");
 		
 		setResultView(resultView);
 	}
 	
 	public IRollResultView getResultView() {
+		System.out.println("RollController getResultView()");
 		return resultView;
 	}
 	public void setResultView(IRollResultView resultView) {
+		System.out.println("RollController setResultView()");
 		this.resultView = resultView;
 	}
 
 	public IRollView getRollView() {
+		System.out.println("RollController getRollView()");
 		return (IRollView)getView();
 	}
 	
 	@Override
 	public void rollDice() {
-
+		System.out.println("RollController rollDice()");
 		getResultView().showModal();
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("RollController update()");
 	}
 
 }
