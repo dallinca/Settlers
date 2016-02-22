c10b67c3f1c320e4502c3571bd750ca076d8ebfd
John Christopher Wiedemeier <jchrisw@colorado.cs.byu.edu>
2016-02-22 15:13:55 -0700

    odds n ends

--- test/client/communication/ServerProxyTest.java
+++ test/client/communication/ServerProxyTest.java
@@ -2,7 +2,6 @@ package client.communication;
 
 import static org.junit.Assert.*;
 
-
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +9,8 @@ import org.junit.Test;
 import shared.communication.params.move.*;
 import shared.communication.params.nonmove.*;
 import shared.communication.params.move.devcard.*;
+import shared.communication.results.nonmove.Login_Result;
+import shared.communication.results.nonmove.Register_Result;
 import client.ClientException;
 import client.proxy.*;
 
@@ -33,12 +34,37 @@ public class ServerProxyTest {
 	}
 
 	@Test
-	public void testConnection() throws ClientException {
-		//System.out.println("Testing connection.");
+	public void testLogin() throws ClientException {		
+		Login_Result result = prox.login(new Login_Params("Sam", "sam"));
 		
-		Object result = prox.doPost("/user/login", new Login_Params("Sam", "sam"));
+		
+		assertTrue(result.isValid());
+		assertEquals("Sam", result.getName());
+		assertEquals(0, result.getId());
+		
+		result = prox.login(new Login_Params("sam", "sam"));
+		
+		assertFalse(result.isValid());
+	}
 	
-		//System.out.println(result.toString());
+	@Test
+	public void testRegister() throws ClientException {
+
+		Register_Result regResult = prox.register(new Register_Params("Tiger", "Shark"));
+		
+		assertTrue(regResult.isValid());
+		assertEquals("Tiger", regResult.getName());
+		assertEquals(12, regResult.getId());
+		
+		regResult = prox.register(new Register_Params("Tiger", "Shark"));
+		
+		assertFalse(regResult.isValid());
+		
+		Login_Result result = prox.login(new Login_Params("Tiger", "Shark"));
+		
+		assertTrue(result.isValid());
+		assertEquals("Tiger", result.getName());
+		assertEquals(12, result.getId());
 	}
 	
 	@Test
@@ -47,14 +73,27 @@ public class ServerProxyTest {
 		
 		//System.out.println("Testing all commands.");
 		//At this stage, all commands return 400's because of their empty states.
-		
+		ServerProxy prox1 = new ServerProxy();
+		ServerProxy prox2 = new ServerProxy();
+		ServerProxy prox3 = new ServerProxy();
+		ServerProxy prox4 = new ServerProxy();
+			
 		try{
+			//Sam, Brooke, Pete, Mark
+		System.out.println("Testing commands.");
+		
+		prox1.login(new Login_Params("Sam", "sam"));
+		prox2.login(new Login_Params("Brooke", "brooke"));
+		prox3.login(new Login_Params("Pete", "pete"));
+		prox4.login(new Login_Params("Mark", "mark"));
+		
+		
+		
+		
+		prox.joinGame(new Join_Params());
 			
-		prox.register(new Register_Params("Tiger", "Shark"));
-		prox.login(new Login_Params("Tiger", "Shark"));
-			
-		prox.sendChat(new SendChat_Params());
-		prox.rollNumber(new RollNumber_Params());
+		prox.sendChat(new SendChat_Params(0, "Yo bro how's it going???"));
+		prox.rollNumber(new RollNumber_Params(8));
 		prox.robPlayer(new RobPlayer_Params());
 		prox.playYearOfPlenty(new PlayYearOfPlenty_Params());
 		prox.playSoldier(new PlaySoldier_Params());
@@ -65,8 +104,8 @@ public class ServerProxyTest {
 		prox.maritimeTrade(new MaritimeTrade_Params());
 		prox.login(new Login_Params());
 		prox.listGames(new List_Params());
-		prox.listAI(new ListAI_Params());
-		prox.joinGame(new Join_Params());
+		//prox.listAI(new ListAI_Params());
+		
 		prox.getVersion(new GetVersion_Params());
 		prox.finishTurn(new FinishTurn_Params());
 		prox.discardCards(new DiscardCards_Params());
@@ -75,7 +114,7 @@ public class ServerProxyTest {
 		prox.buildSettlement(new BuildSettlement_Params());
 		prox.buildRoad(new BuildRoad_Params());
 		prox.buildCity(new BuildCity_Params());
-		prox.addAI(new AddAI_Params());
+		//prox.addAI(new AddAI_Params());
 		prox.acceptTrade(new AcceptTrade_Params());
 		}
 		
