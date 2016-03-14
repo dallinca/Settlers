5dc56b0bd55519247492da2438984382410d92c8
Dallin Andersen <andersen.dallin@gmail.com>
2016-03-14 10:51:12 -0600

    added some methods to the IserverFacade and the serverFacades prepping for javadoc

--- src/server/facade/IServerFacade.java
+++ src/server/facade/IServerFacade.java
@@ -1,5 +1,41 @@
 package server.facade;
 
-public class IServerFacade {
+import java.util.ArrayList;
+import java.util.List;
 
+import shared.model.Game;
+
+/**
+ * TODO -- Javadoc
+ * 
+ * @author Dallin
+ *
+ */
+public interface IServerFacade {
+	
+	List<Game> liveGames = new ArrayList<Game>();
+
+	public boolean acceptTrade();
+	public boolean buildCity();
+	public boolean buildRoad();
+	public boolean buildSettlement();
+	public boolean buyDevCard();
+	public boolean discardCards();
+	public boolean finishTurn();
+	public boolean maritimeTrade();
+	public boolean offerTrade();
+	public boolean robPlayer();
+	public boolean rollNumber();
+	public boolean sendChat();
+	
+	// Dev Card play actions
+	public boolean playMonopoly();
+	public boolean playMonument();
+	public boolean playRoadBuilding();
+	public boolean playSoldier();
+	public boolean playYearOfPlenty();
+	
+	// Return the game that the command is meant to operate on
+	Game findGameForCommand(); 
+	
 }
--- src/server/facade/ServerFacade.java
+++ src/server/facade/ServerFacade.java
@@ -1,5 +1,115 @@
-package server;
+package server.facade;
 
-public class ServerFacade {
+import shared.model.Game;
+
+public class ServerFacade implements IServerFacade {
+
+	@Override
+	public boolean acceptTrade() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean buildCity() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean buildRoad() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean buildSettlement() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean buyDevCard() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean discardCards() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean finishTurn() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean maritimeTrade() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean offerTrade() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean robPlayer() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean rollNumber() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean sendChat() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean playMonopoly() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean playMonument() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean playRoadBuilding() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean playSoldier() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean playYearOfPlenty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Game findGameForCommand() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
