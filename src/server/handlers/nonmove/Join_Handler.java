e1843edac18c356c43e249cec93bbbbadc9f821b
Thomas Wilkinson <jazz.wookie@gmail.com>
2016-03-14 11:13:52 -0600

    the rest of the nonmove classes have implemented httphandler

--- src/server/handlers/nonmove/Join_Handler.java
+++ src/server/handlers/nonmove/Join_Handler.java
@@ -1,5 +1,16 @@
 package server.handlers.nonmove;
 
-public class Join_Handler {
+import java.io.IOException;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class Join_Handler implements HttpHandler {
+
+	@Override
+	public void handle(HttpExchange arg0) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/server/handlers/nonmove/Register_Handler.java
+++ src/server/handlers/nonmove/Register_Handler.java
@@ -1,5 +1,16 @@
 package server.handlers.nonmove;
 
-public class Register_Handler {
+import java.io.IOException;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class Register_Handler implements HttpHandler {
+
+	@Override
+	public void handle(HttpExchange arg0) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
