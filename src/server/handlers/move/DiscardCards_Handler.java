c32acf52ede462f1a169483d5aad216943ef66c6
Thomas Wilkinson <jazz.wookie@gmail.com>
2016-03-14 11:11:10 -0600

    implemented HttpHandler on all move and dev card classes

--- src/server/handlers/move/DiscardCards_Handler.java
+++ src/server/handlers/move/DiscardCards_Handler.java
@@ -1,5 +1,18 @@
 package server.handlers.move;
 
-public class DiscardCards_Handler {
+import java.io.IOException;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class DiscardCards_Handler implements HttpHandler{
+
+	@Override
+	public void handle(HttpExchange arg0) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
 }
--- src/server/handlers/move/RobPlayer_Handler.java
+++ src/server/handlers/move/RobPlayer_Handler.java
@@ -1,5 +1,17 @@
 package server.handlers.move;
 
-public class RobPlayer_Handler {
+import java.io.IOException;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class RobPlayer_Handler implements HttpHandler {
+
+	@Override
+	public void handle(HttpExchange arg0) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 }
