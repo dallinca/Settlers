fc3b8dcdd1341d33118c1ac911f1feb6c0cc4d15
Dallin Andersen <andersen.dallin@gmail.com>
2016-03-14 10:06:29 -0600

    added framing and javadoc for the dev card command classes

--- src/server/commands/move/devcard/PlayRoadBuilding_Command.java
+++ src/server/commands/move/devcard/PlayRoadBuilding_Command.java
@@ -1,5 +1,42 @@
 package server.commands.move.devcard;
 
-public class PlayRoadBuilding_Command {
+import server.commands.Command;
+import shared.model.Game;
+
+/**
+ * Concrete command implementing the Command interface.
+ * Issues the Play Road Building Dev Card action on the server game model.
+ * 
+ * @author Dallin
+ *
+ */
+public class PlayRoadBuilding_Command implements Command {
+	private Game game;
+
+	/**
+	 * Non-standard command pattern constructor instantiation without the game model.
+	 * The game model will be determined after original command instantiation.
+	 * 
+	 */
+	public PlayRoadBuilding_Command() {}
+	
+	/**
+	 * Standard Command pattern constructor instantiation with the game model
+	 * 
+	 * @param game
+	 */
+	public PlayRoadBuilding_Command(Game game) {
+		this.game = game;
+	}
+
+	/**
+	 * TODO -- javadoc
+	 * 
+	 */
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
