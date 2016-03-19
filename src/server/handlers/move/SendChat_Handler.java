890fb7daa3b368b27caf19afd7368142b71228b1
John Christopher Wiedemeier <jchrisw@carolina.cs.byu.edu>
2016-03-18 20:35:23 -0600

    HANDLERS

--- src/server/handlers/move/SendChat_Handler.java
+++ src/server/handlers/move/SendChat_Handler.java
@@ -24,60 +24,57 @@ import com.sun.net.httpserver.HttpHandler;
 public class SendChat_Handler extends SettlersOfCatanHandler {
 
 	private Logger logger = Logger.getLogger("settlers-of-catan"); 	
-	
+
 	/**
 	 * Receives the SendChat Params and constructs the appropriate command and passes it to the Server Facade after decoding the object.
 	 */
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
-			
+
 		logger.entering("server.handlers.SendChat", "handle");
 		//Handling Login http exchange.
 
 		String job;	
 		SendChat_Params request;
 		SendChat_Result result;
-		
+
 		LinkedList<String> cookies = extractCookies(exchange);
-		
+
 		String check = validateCookies(cookies);		
-		
+
 		if (check.equals("VALID")){
-			
+
 			User user = gson.fromJson(cookies.getFirst(), User.class);	
 			int gameID = Integer.parseInt(cookies.get(1));
-			
+
 			job = getExchangeBody(exchange); //get json string from exchange.
-			request = gson.fromJson(job, SendChat_Params.class); //deserialize request from json				
+			request = gson.fromJson(job, SendChat_Params.class); //deserialize request from json
+
 			SendChat_Command command = new SendChat_Command(request, gameID, user.getPlayerID());
-			
-			command
-			
-				
-			result = facade.list(request);//Call facade to perform operation with request
-			
-			exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0); //Everything's okay
-			job = gson.toJson(result);	//serialize result to json			
-			
+
+			command.execute();//Execute the command	
+
+			result = command.getResult(); //Get result from command			
+
+			if (result.isValid()){
+				exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0); //Everything's okay
+				job = gson.toJson(result);	//serialize result to json	
+			}else{
+				exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_REQUEST, 0);
+				job = "COMMAND FAILURE";	
+			}			
 		}else{
 			job = check;			
 			exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_REQUEST, 0); //User invalid			
 		}		
-		
+
 		OutputStreamWriter sw = new OutputStreamWriter(exchange.getResponseBody());
 		sw.write(job);//Write result to stream.
 		sw.flush();	
 
 		exchange.getResponseBody().close();		
 		logger.exiting("server.handlers.SendChat", "handle");	
-		
-		
-		
-		
-		
-		
-		
-		
+
 	}
 
 }
