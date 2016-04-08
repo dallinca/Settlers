bf711434c64319823ffd1d758e1db171806a9140
Scott Gary Vaughan <scottgv@galaga.cs.byu.edu>
2016-04-08 15:27:39 -0600

    factory

---
+++ src/server/persistenceprovider/AbstractFactory.java
@@ -0,0 +1,13 @@
+package server.persistenceprovider;
+
+public class AbstractFactory {
+
+	PersistenceProviderInterface getPersistenceProvider(){
+		
+		return null;		
+	}
+	
+	void specifyPlugin(){
+		
+	}
+}
